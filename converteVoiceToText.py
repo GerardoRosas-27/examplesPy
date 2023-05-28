140d33a7b1f2c01399f44881a671d1c74105d608
Gerardo <gerardo.rosas@external.danone.com>
2023-05-27 22:15:48 -0600

    add converte voice to text

--- converteVoiceToText.py
+++ converteVoiceToText.py
@@ -1,23 +1,59 @@
-import speech_recognition as sr
+import whisper
+
+import pyaudio
+import wave
 from pydub import AudioSegment
 
-# Convertir el archivo MP3 a WAV
-audio_mp3 = "audio.mp3"
-audio_wav = "audio_convertido.wav"
-sound = AudioSegment.from_mp3(audio_mp3)
-sound.export(audio_wav, format="wav")
 
-# Inicializar el reconocedor de voz
-r = sr.Recognizer()
+def converteVoiceToText():
+    model = whisper.load_model("base")
+    result = model.transcribe("audio1.mp3")
+    print(result["text"])
 
-# Cargar el archivo WAV y convertirlo a texto
-with sr.AudioFile(audio_wav) as source:
-    audio_data = r.record(source)
-    try:
-        # Configurar el idioma a inglés (en-us) y realizar la conversión a texto
-        texto = r.recognize_google(audio_data, language="en-US")
-        print("Texto convertido: ", texto)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition no pudo entender el audio")
-    except sr.RequestError as e:
-        print("No se pudo solicitar resultados a Google Speech Recognition; {0}".format(e))
+
+def grabar_audio(nombre_archivo, duracion):
+    chunk = 1024
+    formato = pyaudio.paInt16
+    canales = 2
+    tasa_muestreo = 44100
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=formato,
+                    channels=canales,
+                    rate=tasa_muestreo,
+                    input=True,
+                    frames_per_buffer=chunk)
+
+    print("Grabando audio...")
+
+    frames = []
+
+    for i in range(0, int(tasa_muestreo / chunk * duracion)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("Grabación finalizada.")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # Guardar el archivo de audio en formato WAV
+    wf = wave.open(nombre_archivo, 'wb')
+    wf.setnchannels(canales)
+    wf.setsampwidth(p.get_sample_size(formato))
+    wf.setframerate(tasa_muestreo)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    # Convertir el archivo WAV a MP3
+    audio = AudioSegment.from_wav(nombre_archivo)
+    audio.export(nombre_archivo + '.mp3', format='mp3')
+
+    print("El audio se ha guardado como:", nombre_archivo + '.mp3')
+
+# Ejemplo de uso: grabar audio durante 5 segundos y guardarlo como 'grabacion.mp3'
+grabar_audio('grabacion', 5)
+#converteVoiceToText()
+
